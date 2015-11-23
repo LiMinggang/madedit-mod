390203edf4e9608cae68e44e012981e4c5eb3942
minggang.li@gmail.com <minggang.li@gmail.com>
2015-11-23 17:04:22 +0800

    Update About information

--- src/MadAboutDialog.cpp
+++ src/MadAboutDialog.cpp
@@ -33,12 +33,12 @@ END_EVENT_TABLE()
 wxString g_MadEdit_URL(wxT("http://sourceforge.net/projects/madedit/ or http://sourceforge.net/projects/madedit-mod/"));
 wxString g_MadEditMod_URL(wxT("https://sourceforge.net/p/madedit-mod/wiki/MadEdi-Mod/"));
 wxString g_MadEditModLicense (
-	_("    This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.\n\
-	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\
-	You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.\n")
+	_("This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.\n\n\
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\n\
+You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.\n")
 );
 wxString g_MadEditModCredits(
-	_("Minggang Li(Current maintainer of MadEdit-Mod)\n\
+	_("Minggang Li(Current maintainer of MadEdit-Mod)\n\n\
 Alston Chen(Author of MadEdit)\n\
 JiaYanwei(Patch contributer for MadEdit)\n\
 nikoss(Greek Translation)\n\
