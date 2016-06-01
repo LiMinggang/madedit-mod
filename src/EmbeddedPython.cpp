8d0a677a75723c7e10d0e96d908d796ed52a6d98
minggang.li@gmail.com <minggang.li@gmail.com>
2016-06-01 23:47:45 +0800

    Import msgbox

--- src/EmbeddedPython.cpp
+++ src/EmbeddedPython.cpp
@@ -62,7 +62,7 @@ EmbeddedPython::EmbeddedPython()
             "sys.stderr = redirector.stderr()\n"
             "from madpython import MadEdit, MadEditCommand, MadWordWrapMode, MadEditMode, "
             "MadCaretType, MadNewLineType, MadConvertEncodingFlag, MadNumberingStepType, "
-            "MadNumberFormat, MadNumberAlign";
+            "MadNumberFormat, MadNumberAlign, MsgBox, InputDlgBox";
 
 #if 0
         /* FixMe: exception thrown, mmh - seems a bug in boost.python, see
