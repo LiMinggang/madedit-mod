62bea2ad6e81bdaa8a746755b6ba6e6b05bf827d
minggang.li@gmail.com <minggang.li@gmail.com>
2016-06-21 17:21:45 +0800

    Minor improvement

--- minipython/Include/pyconfig.h
+++ minipython/Include/pyconfig.h
@@ -3,18 +3,18 @@
 #else
     #if defined(__unix__)
         #if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
-            #if __SIZEOF_LONG__ == 4
-                #include "pyconfig-bsd32.h"
-            #elif __SIZEOF_LONG__ == 8
+            #if __SIZEOF_LONG__ == 8 || defined(__x86_64__)
                 #include "pyconfig-bsd64.h"
+            #elif __SIZEOF_LONG__ == 4
+                #include "pyconfig-bsd32.h"
             #else
                 #error "Unknown LONG size"
             #endif
         #else
-            #if __SIZEOF_LONG__ == 4
-                #include "pyconfig-32.h"
-            #elif __SIZEOF_LONG__ == 8
+            #if __SIZEOF_LONG__ == 8 || defined(__x86_64__)
                 #include "pyconfig-64.h"
+            #elif __SIZEOF_LONG__ == 4
+                #include "pyconfig-32.h"
             #else
                 #error "Unknown LONG size"
             #endif
