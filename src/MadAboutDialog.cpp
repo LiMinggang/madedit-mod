c0badb5add78107ac34fff0e2fa70b63db8fd9d2
minggang.li@gmail.com <minggang.li@gmail.com>
2016-06-22 20:15:25 +0800

    Update license string on About dialog

--- src/MadAboutDialog.cpp
+++ src/MadAboutDialog.cpp
@@ -35,10 +35,15 @@ END_EVENT_TABLE()
 wxString g_MadEdit_URL(wxT("http://sourceforge.net/projects/madedit/ or http://sourceforge.net/projects/madedit-mod/"));
 wxString g_MadEditMod_URL(wxT("https://sourceforge.net/p/madedit-mod/wiki/MadEdi-Mod/"));
 wxString g_MadEditModLicense (
-_("Copyright (C) 2012-2015  Minggang Li <minggang.li@gmail.com>\n\n\
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.\n\n\
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\n\
-You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.\n")
+_("Copyright (C) 2012-2016  Minggang Li <minggang.li@gmail.com>\n\n\
+    This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by\
+ the Free Software Foundation, either version 3 of the License, or\
+ (at your option) any later version.\n\n\
+    This program is distributed in the hope that it will be useful,\
+ but WITHOUT ANY WARRANTY; without even the implied warranty of\
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.\n\n\
+    You should have received a copy of the GNU General Public License\
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.\n")
 );
 wxString g_MadEditModCredits(
 _("Minggang Li\t\tCurrent maintainer of MadEdit-Mod\n\
